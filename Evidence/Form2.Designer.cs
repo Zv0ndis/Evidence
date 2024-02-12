2ad19855cb0a4ce11e2224c5294e55a54ec8bc28
Tomáš Zvoníček <zvonicek.tomas@spszl.cz>
2024-02-12 11:35:23 +0100

    dodelat se musi

--- Evidence/Form2.Designer.cs
+++ Evidence/Form2.Designer.cs
@@ -28,18 +28,227 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelSurname = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.maskedTextBoxPoints = new System.Windows.Forms.MaskedTextBox();
+            this.labelPoints = new System.Windows.Forms.Label();
+            this.radioButtonAccepted = new System.Windows.Forms.RadioButton();
+            this.radioButtonDenied = new System.Windows.Forms.RadioButton();
+            this.labelState = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.labelAverage = new System.Windows.Forms.Label();
+            this.maskedTextBoxAverage = new System.Windows.Forms.MaskedTextBox();
             this.SuspendLayout();
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(41, 53);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 0;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(38, 37);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "Name";
+            // 
+            // labelSurname
+            // 
+            this.labelSurname.AutoSize = true;
+            this.labelSurname.Location = new System.Drawing.Point(144, 37);
+            this.labelSurname.Name = "labelSurname";
+            this.labelSurname.Size = new System.Drawing.Size(49, 13);
+            this.labelSurname.TabIndex = 3;
+            this.labelSurname.Text = "Surname";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(147, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "DoB";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(41, 92);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(149, 20);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(41, 131);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Obor";
+            // 
+            // maskedTextBoxPoints
+            // 
+            this.maskedTextBoxPoints.Location = new System.Drawing.Point(41, 171);
+            this.maskedTextBoxPoints.Mask = "000";
+            this.maskedTextBoxPoints.Name = "maskedTextBoxPoints";
+            this.maskedTextBoxPoints.Size = new System.Drawing.Size(25, 20);
+            this.maskedTextBoxPoints.TabIndex = 9;
+            this.maskedTextBoxPoints.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBoxPoints_MaskInputRejected);
+            // 
+            // labelPoints
+            // 
+            this.labelPoints.AutoSize = true;
+            this.labelPoints.Location = new System.Drawing.Point(38, 155);
+            this.labelPoints.Name = "labelPoints";
+            this.labelPoints.Size = new System.Drawing.Size(36, 13);
+            this.labelPoints.TabIndex = 10;
+            this.labelPoints.Text = "Points";
+            // 
+            // radioButtonAccepted
+            // 
+            this.radioButtonAccepted.AutoSize = true;
+            this.radioButtonAccepted.Location = new System.Drawing.Point(41, 210);
+            this.radioButtonAccepted.Name = "radioButtonAccepted";
+            this.radioButtonAccepted.Size = new System.Drawing.Size(71, 17);
+            this.radioButtonAccepted.TabIndex = 11;
+            this.radioButtonAccepted.TabStop = true;
+            this.radioButtonAccepted.Text = "Accepted";
+            this.radioButtonAccepted.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonDenied
+            // 
+            this.radioButtonDenied.AutoSize = true;
+            this.radioButtonDenied.Location = new System.Drawing.Point(118, 210);
+            this.radioButtonDenied.Name = "radioButtonDenied";
+            this.radioButtonDenied.Size = new System.Drawing.Size(59, 17);
+            this.radioButtonDenied.TabIndex = 12;
+            this.radioButtonDenied.TabStop = true;
+            this.radioButtonDenied.Text = "Denied";
+            this.radioButtonDenied.UseVisualStyleBackColor = true;
+            // 
+            // labelState
+            // 
+            this.labelState.AutoSize = true;
+            this.labelState.Location = new System.Drawing.Point(38, 194);
+            this.labelState.Name = "labelState";
+            this.labelState.Size = new System.Drawing.Size(82, 13);
+            this.labelState.TabIndex = 13;
+            this.labelState.Text = "State of student";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(291, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(71, 17);
+            this.radioButton1.TabIndex = 15;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "University";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(49, 12);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(112, 17);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Secondary School";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // labelAverage
+            // 
+            this.labelAverage.AutoSize = true;
+            this.labelAverage.Location = new System.Drawing.Point(80, 155);
+            this.labelAverage.Name = "labelAverage";
+            this.labelAverage.Size = new System.Drawing.Size(47, 13);
+            this.labelAverage.TabIndex = 17;
+            this.labelAverage.Text = "Average";
+            // 
+            // maskedTextBoxAverage
+            // 
+            this.maskedTextBoxAverage.Location = new System.Drawing.Point(79, 171);
+            this.maskedTextBoxAverage.Mask = "000";
+            this.maskedTextBoxAverage.Name = "maskedTextBoxAverage";
+            this.maskedTextBoxAverage.Size = new System.Drawing.Size(22, 20);
+            this.maskedTextBoxAverage.TabIndex = 16;
             // 
             // Form2
             // 
             this.ClientSize = new System.Drawing.Size(387, 277);
+            this.Controls.Add(this.labelAverage);
+            this.Controls.Add(this.maskedTextBoxAverage);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.labelState);
+            this.Controls.Add(this.radioButtonDenied);
+            this.Controls.Add(this.radioButtonAccepted);
+            this.Controls.Add(this.labelPoints);
+            this.Controls.Add(this.maskedTextBoxPoints);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelSurname);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.textBoxName);
             this.Name = "Form2";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form2_FormClosed);
+            this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelSurname;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxPoints;
+        private System.Windows.Forms.Label labelPoints;
+        private System.Windows.Forms.RadioButton radioButtonAccepted;
+        private System.Windows.Forms.RadioButton radioButtonDenied;
+        private System.Windows.Forms.Label labelState;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label labelAverage;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxAverage;
     }
 }
 
--- Evidence/Form2.cs
+++ Evidence/Form2.cs
@@ -18,11 +18,33 @@ namespace Evidence
             InitializeComponent();
         }
 
+        
         private void Form2_FormClosed(object sender, FormClosedEventArgs e)
         {
             // Zobrazíme Form1
-            Form form1 = new Form();
+            Form form1 = new FormsMain();
             form1.Show();
+        }
+
+        private void maskedTextBoxPoints_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            maskedTextBoxAverage.Visible = false;
+            labelAverage.Visible = false;
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            maskedTextBoxAverage.Visible = true;
+            labelAverage.Visible = true;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            radioButton2.Checked = true;
         }
     }
 }
